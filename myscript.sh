9f150e8e300bc86ea5b208a627428393d01733b5
Yalda <yalda.rahmati@gmail.com>
2024-11-24 17:58:35 -0500

    test

--- myscript.sh
+++ myscript.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-echo "This is a why i do not one experiment"
-=======
-echo "This is a i do not know why one experiment"
->>>>>>> 2b9fea197f37fe818aafd0bf0f6973b34c700f78
+echo "This is a why i do not know the experiment"
